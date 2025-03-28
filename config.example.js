8a6d95627feaa12a96bee9587a4f8be6df73b7d0
aitor santaeugenia mari <aitorsantaeugenia@gmail.com>
2025-03-28 13:06:13 +0100

    feat: add JavaScript configuration template

---
+++ config.example.js
@@ -0,0 +1,10 @@
+// Configuración de Firebase para el navegador
+export const firebaseConfig = {
+  apiKey: "your_api_key_here",
+  authDomain: "your_auth_domain_here",
+  projectId: "your_project_id_here",
+  storageBucket: "your_storage_bucket_here",
+  messagingSenderId: "your_messaging_sender_id_here",
+  appId: "your_app_id_here",
+  measurementId: "your_measurement_id_here"
+}; 
